86a7c0f20b9755ef7a4a63a94fdd50f7582c6814
PC1 <umdomby@yandex.ru>
2024-11-18 19:05:13 +0300

    add page /device add api/devic add Table shadcn

--- components/shared/device.tsx
+++ components/shared/device.tsx
@@ -4,6 +4,16 @@ import {Device} from '@prisma/client';
 import React, {useEffect} from 'react';
 import {Api} from "@/shared/services/api-client";
 
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
 export const Devices: React.FC = () => {
 
     const [device, setDevice] = React.useState<Device[]>([]);
@@ -24,13 +34,28 @@ export const Devices: React.FC = () => {
     return (
 
         <div>
-            {device.map((device) => (
-                <div
-                    key={device.id}
-                    className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10">
-                    <span>{device.name}</span>
-                </div>
-            ))}
+            <Table>
+                <TableCaption>Need For Speed records</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[400px]">GAME</TableHead>
+                        <TableHead>Map</TableHead>
+                        <TableHead >User</TableHead>
+                        <TableHead className="text-right">Time</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+
+                {device.map((device) => (
+                    <TableRow key={device.id}>
+                        <TableCell className="font-medium">{device.name}</TableCell>
+                        <TableCell>{device.description}</TableCell>
+                        <TableCell>{device.username}</TableCell>
+                        <TableCell className="text-right">{device.timestate.substring(3)}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
         </div>
 
     );
